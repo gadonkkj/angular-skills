413284b2961bcb82c2db8a28117bcf01e9d9f052
brandon.dev <101531074+gadonkkj@users.noreply.github.com>
2022-09-25 23:56:19 -0300

    Update card.component.ts

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
+  
   @Input() card;
-  constructor() { }
-
+  constructor(private httpClient: HttpClient) { }
+  color = "white"
   ngOnInit() {
   }
 
   onLike(card: any){
-    // TODO: incrementar o like, salvar via rest
+      card.likes += 1
+      this.httpClient.post('/api/skills', card).subscribe((ret: Array<any>) =>{
+        console.log(this.card.likes)
+      });
+      if( card.likes >=10){
+          this.color="accent"
+      }else if( card.likes >=5){
+        this.color="primary"
+      }
   }
 
+  
+ 
   onShare(card: any){
-    // TODO: abrir o link do seu linkedin
+    window.open("https://www.linkedin.com/in/brandon-menger-162036243/", '_blank').focus();
   }
 
 }
